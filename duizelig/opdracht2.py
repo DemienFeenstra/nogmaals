950f49af87f72bb11c1ce9723f140546498673d4
DemiënFeenstra <99064788@mydavinci.nl>
2021-11-15 11:52:10 +0100

    opdracht 2 ingekort

--- duizelig/opdracht2.py
+++ duizelig/opdracht2.py
@@ -3,12 +3,8 @@ i = 1
 while i <= 12:
         print(i, "am")
         i = i + 1
-        if i == 0:
-            break
-        
+
 j = 1
 while j <= 12:
         print(j, "pm")
         j = j + 1
-        if j == 0:
-            break
