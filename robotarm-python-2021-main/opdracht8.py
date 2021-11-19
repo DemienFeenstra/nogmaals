4a6b371e2393fdc5edb11acb8eed5be31349e664
DemiënFeenstra <99064788@mydavinci.nl>
2021-11-19 21:22:31 +0100

    opdracht 8

--- robotarm-python-2021-main/opdracht8.py
+++ robotarm-python-2021-main/opdracht8.py
@@ -1,9 +1,17 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 1')
+robotArm = RobotArm('exercise 8')
 
 # Jouw python instructies zet je vanaf hier:
 
+robotArm.moveRight()
+for i in range(7):
+    robotArm.grab()
+    for j in range(8):
+        robotArm.moveRight()
+    robotArm.drop()
+    for k in range(8):
+        robotArm.moveLeft()
 
 
 
