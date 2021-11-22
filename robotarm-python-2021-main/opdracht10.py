03d05c71aac2f75a16cbacce49c6e669ead11ba0
DemiënFeenstra <99064788@mydavinci.nl>
2021-11-22 09:35:31 +0100

    opdracht 10

--- robotarm-python-2021-main/opdracht10.py
+++ robotarm-python-2021-main/opdracht10.py
@@ -1,12 +1,16 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 1')
+robotArm = RobotArm('exercise 10')
 
 # Jouw python instructies zet je vanaf hier:
 
-
-
-
+for i in range(9, 0, -2):
+    robotArm.grab()
+    for j in range(i):
+        robotArm.moveRight()
+    robotArm.drop()
+    for k in range(i - 1):
+        robotArm.moveLeft()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
