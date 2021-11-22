5cba72ce7f37de9e817c803ce4e48b236399c07f
DemiënFeenstra <99064788@mydavinci.nl>
2021-11-22 09:42:00 +0100

    opdracht 11

--- robotarm-python-2021-main/opdracht11.py
+++ robotarm-python-2021-main/opdracht11.py
@@ -1,8 +1,21 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 1')
+robotArm = RobotArm('exercise 11')
 
 # Jouw python instructies zet je vanaf hier:
+
+for i in range(10):
+    robotArm.moveRight()
+for j in range(10):
+    robotArm.grab()
+    color = robotArm.scan()
+    if color == "white":
+        robotArm.moveRight()
+        robotArm.drop()
+        robotArm.moveLeft()
+    else:
+        robotArm.drop()
+    robotArm.moveLeft()
 
 
 
