98aa5bc97f31619cbb2a92a36e1d600369fab0c1
DemiënFeenstra <99064788@mydavinci.nl>
2021-11-22 10:31:03 +0100

    opdracht 12

--- robotarm-python-2021-main/opdracht12.py
+++ robotarm-python-2021-main/opdracht12.py
@@ -1,12 +1,28 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 1')
+robotArm = RobotArm('exercise 12')
 
 # Jouw python instructies zet je vanaf hier:
 
+for i in range(8):
+    robotArm.moveRight()
 
+for i in range(1,10):
+    robotArm.grab()
+    scan = robotArm.scan()
+    if scan == "red":
+        
+        for j in range(i):
+            robotArm.moveRight()
+        robotArm.drop() 
 
+        for j in range(i):
+            robotArm.moveLeft()  
+    else:
+        robotArm.drop()
+    robotArm.moveLeft()
 
+    
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
